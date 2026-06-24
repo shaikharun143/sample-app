fe1480a581ba9ad75e7ecd92e3024e5a7d5e5f3b
Shaik Harun Yahya <146335426+shaikharun143@users.noreply.github.com>
2026-06-24 13:17:14 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from	app	import	app
 def	test_home():
   res	=	app.test_client().get("/")
   assert	res.status_code	==	200
-  assert	b"Hello"	in	res.data
+  assert	b"Hello" in	res.data
   
 def	test_health():
   res	=	app.test_client().get("/health")

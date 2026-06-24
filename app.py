00075955079030c6f145a38479c7ab89479b5dae
Shaik Harun Yahya <146335426+shaikharun143@users.noreply.github.com>
2026-06-24 13:18:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ def home():
 @app.route("/health")
 def health():
     return {"status": "healthy"}, 200
+    
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
